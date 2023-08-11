f98c21576758a29c180c3468a8b038a5ff8d0758
E. Hannah <echannah631@gmail.com>
2023-08-11 14:18:41 -0500

    refactored. added properties and age validation. used validation in methods

--- Profile.cs
+++ Profile.cs
@@ -3,44 +3,79 @@ namespace MatchMakerService
 {
 	public class Profile
 	{
-        private string name;
+		//Fields
 		private int age;
-		private string city;
-		private string country;
-		private string pronouns;
-		private string[] hobbies;
+		private bool isOfAge;
 
+		//Properties
+		public string? Name { get; set; }
+		public int Age
+		{
+			get { return age; }
+			set { if (value < 18)
+				{
+					isOfAge = false;
+				}
+				else {
+					isOfAge = true;
+					age = value;
+				} }
+		}
+		public string? City { get; set; }
+		public string? Country { get; set; }
+		public string? Pronouns { get; set; }
+		public string[]? Hobbies { get; set; }
+
+		//Constructors
         public Profile(string name, int age, string city, string country, string pronouns = "they/them")
 		{
-			this.name = name;
-			this.age = age;
-			this.city = city;
-			this.country = country;
-			this.pronouns = pronouns;
-			this.hobbies = new string[4];
+            Name = name;
+            Age = age;
+			City = city;
+			Country = country;
+			Pronouns = pronouns;
+			Hobbies = new string[0];
 		}
 
+		//Methods
 		public string ViewProfile()
 		{
-			string info = $"Name: {name}\nAge: {age}\nLocation: {city}, {country}\nPronouns: {pronouns}\nHobbies: ";
-			foreach (string hobby in hobbies)
+			if (this.isOfAge)
 			{
-				if (hobby == hobbies[hobbies.Length - 1])
-				{
-					info += hobby;
-				} else
-				{
-                    info += hobby + ", ";
+                string info = $"Name: {Name}\nAge: {age}\nLocation: {City}, {Country}\nPronouns: {Pronouns}";
+                if (Hobbies is not null && Hobbies.Length > 0)
+                {
+                    info += "\nHobbies: ";
+                    foreach (string hobby in Hobbies)
+                    {
+                        if (hobby == Hobbies[Hobbies.Length - 1])
+                        {
+                            info += hobby;
+                        }
+                        else
+                        {
+                            info += hobby + ", ";
+                        }
+                    }
                 }
-				
-			}
-			return info;
+
+                return info;
+            }
+
+			return "Not of age";
 		}
 
 		public void SetHobbies(string[] hobbies)
 		{
-			this.hobbies = hobbies;
-		}
+			if (isOfAge )
+			{
+                Hobbies = hobbies;
+            } else
+			{
+				Console.WriteLine("Cannot set hobbies. Too young for match making service");
+			}
+
+        }
 	}
 }
 
--- Program.cs
+++ Program.cs
@@ -7,9 +7,13 @@ namespace MatchMakerService
         static void Main(string[] args)
         {
             Profile sam = new Profile("Sam Drakkila", 30, "New York", "USA", "he/him");
+            Console.WriteLine(sam.ViewProfile());
             sam.SetHobbies(new string[] { "listening to audiobooks and podcats", "playing rec sports like basketball and raquetball", "writing a speculative fiction novel", "reading advice columns" });
             Console.WriteLine(sam.ViewProfile());
-
+            Console.WriteLine();
+            Profile jaxson = new Profile("Jaxson Sanders", 15, "Wyoming", "USA");
+            Console.WriteLine(jaxson.ViewProfile());
+            jaxson.SetHobbies(new string[] { "listening to audiobooks and podcats", "playing rec sports like basketball and raquetball", "writing a speculative fiction novel", "reading advice columns" });
         }
     }
 }
